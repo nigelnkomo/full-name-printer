50b68a9c04fdb917b37090a747cdcfacb333400f
nigel nkomo <nigel.nkomo@icloud.com>
2024-06-19 14:09:46 +0200

    Add user prompts

--- src/full_name_printer.py
+++ src/full_name_printer.py
@@ -1,4 +1,4 @@
-def get_formatted_name (last, occupation, *middle):
+def get_formatted_name (last, occupation, middle):
 
     initials = []
 
@@ -10,3 +10,14 @@ def get_formatted_name (last, occupation, *middle):
     full_name = f"{middle_str.title()}. {last.title()}, {occupation.lower()}"
 
     return full_name
+
+if __name__ == "__main__":
+    
+    print ()
+    last = input ("\tKindly enter your family name: ")
+    middle = input ("\tPlease provide your given name, and if you have any, list your middle names separated by spaces: ").split (" ")
+    occupation = input ("\tWhat is your current occupation or profession? ")
+
+    print ()
+    print (f"\tIntroducing {get_formatted_name(last, occupation, middle)}")
+    print ()
